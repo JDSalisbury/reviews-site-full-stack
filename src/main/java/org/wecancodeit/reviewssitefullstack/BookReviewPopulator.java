5658569e7d86e6973adb5d084f83d1a3d02b0fdc
JDSalisbury <salisbury.jeffery@gmail.com>
2018-03-02 16:49:13 -0500

    Added Populator

---
+++ src/main/java/org/wecancodeit/reviewssitefullstack/BookReviewPopulator.java
@@ -0,0 +1,13 @@
+package org.wecancodeit.reviewssitefullstack;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class BookReviewPopulator implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
