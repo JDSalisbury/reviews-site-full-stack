2df715e382fc9a3efdde6bf0226495dcd5a0de24
JDSalisbury <salisbury.jeffery@gmail.com>
2018-03-17 18:31:10 -0400

    added remove button

--- src/main/java/org/wecancodeit/reviewssitefullstack/BookReviewsController.java
+++ src/main/java/org/wecancodeit/reviewssitefullstack/BookReviewsController.java
@@ -59,24 +59,24 @@ public class BookReviewsController {
 		return "tags";
 	}
 	
-	@RequestMapping("/delete-tag")
-	public String deleteIndvidualTag(@RequestParam Long id, @RequestParam String bookReviewTitle) {
-		Logger logger = Logger.getLogger("delete-tag-logger");
-		logger.info("Tag id is " + id + ", and review title is " + bookReviewTitle);
-		Tag tagRemoval = tagRepo.findOne(id);
-		BookReview underReview = bookReviewRepo.findByTitle(bookReviewTitle);
-		logger.info(underReview.getBookTitle());
-		logger.info(tagRemoval.getTag());
-
-		if (tagRemoval != null) {
-			underReview.removeTag(id);
-		}
-		bookReviewRepo.save(underReview);
-		Long animeReviewid = underReview.getId();
-
-		return "redirect:/bookReview?id=" + animeReviewid;
-		
-	}
-	
+//	@RequestMapping("/delete-tag")
+//	public String deleteIndvidualTag(@RequestParam Long id, @RequestParam String bookReviewTitle) {
+//		Logger logger = Logger.getLogger("delete-tag-logger");
+//		logger.info("Tag id is " + id + ", and review title is " + bookReviewTitle);
+//		Tag tagRemoval = tagRepo.findOne(id);
+//		BookReview underReview = bookReviewRepo.findByTitle(bookReviewTitle);
+//		logger.info(underReview.getBookTitle());
+//		logger.info(tagRemoval.getTag());
+//
+//		if (tagRemoval != null) {
+//			underReview.removeTag(id);
+//		}
+//		bookReviewRepo.save(underReview);
+//		Long animeReviewid = underReview.getId();
+//
+//		return "redirect:/bookReview?id=" + animeReviewid;
+//		
+//	}
+//	
 		
 }
--- src/main/resources/static/js/tag.js
+++ src/main/resources/static/js/tag.js
@@ -1,9 +1,32 @@
-const xhr = new XMLHttpRequest()
-xhr.onreadystatechange = function() {
-	if (xhr.readyState === 4 && xhr.status === 200){
-		const res = JSON.parse(xhr.response);
+
+
+document.addEventListener("DOMContentLoaded", function() {
+	// get all of the elements with class removeTag (query selector)
+	const removeTagButtonCollection = document.querySelectorAll('.removeTag')
+	// add event listener to onclick for each to point to removeTag
+	removeTagButtonList = Array.from(removeTagButtonCollection)
+	removeTagButtonList.forEach( function(removeTagButton){
+		removeTagButton.addEventListener('click', removeTag)
+	})
+	
+});
+
+function removeTag(event) {
+	const theButton = event.target
+	const theDivWrappingTheButton = theButton.parentElement
+	console.log(theButton)
+	const id = theButton.dataset.id
+	const title = theButton.dataset.title
+	// do ajax request to /remove-tag?id={id}&bookReviewTitle={title}
+	const xhr = new XMLHttpRequest()//ajax request 
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4 && xhr.status === 200) {
+			console.log(xhr.response);
+//			
+//			// delete the tag from DOM
+			theDivWrappingTheButton.parentElement.removeChild(theDivWrappingTheButton)
+		}
 	}
-	
-	
-	
+	xhr.open('GET', 'http://localhost:8080/delete-tag?id=' + encodeURI(id) + '&bookReviewTitle=' + encodeURI(title),true)
+	xhr.send()
 }
